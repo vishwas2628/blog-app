2556c7188c7da11500de65e412cc9dc2fc507cf9
Vishwas Chourasiya <vishwaschourasiya@gmail.com>
2026-02-08 20:42:15 +0530

    blog home page will go on there

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,5 +1,15 @@
-from django.http import HttpResponse
+
 from django.shortcuts import render
 
+from blogs.models import Blog, Category
+
 def home(request):
-    return render(request , "index.html")
+    categories = Category.objects.all()
+    featured_blog = Blog.objects.filter(is_featured=True)
+    print(featured_blog)
+
+    context =  {
+        'categories' : categories,
+        'featured_blog' : featured_blog,
+    }
+    return render(request , "index.html", context)
