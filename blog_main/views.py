4cc954a3eab862bbc23a234653d3709290217db1
Vishwas Chourasiya <vishwaschourasiya@gmail.com>
2026-02-13 23:02:56 +0530

    development canresume after long days

--- blog_main/views.py
+++ blog_main/views.py
@@ -5,11 +5,12 @@ from blogs.models import Blog, Category
 
 def home(request):
     categories = Category.objects.all()
-    featured_blog = Blog.objects.filter(is_featured=True)
-    print(featured_blog)
+    featured_blog = Blog.objects.filter(is_featured=True,status='Published')
+    blogs = Blog.objects.filter(is_featured=False, status='Published')
 
     context =  {
         'categories' : categories,
         'featured_blog' : featured_blog,
+        'blogs' : blogs,
     }
     return render(request , "index.html", context)
