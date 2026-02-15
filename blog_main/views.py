e2298320956adb2353d555576799f142a8f3b75f
VISHWAS CHOURASIYA <90148043+vishwas2628@users.noreply.github.com>
2026-02-15 12:10:56 +0530

    blog home page will go on there (#1)
    * blog home page will go on there
    
    * development canresume after long days
    
    * further developmwnt in templates "
    add context_processors for categories
    add url for categories nav bar

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,5 +1,14 @@
-from django.http import HttpResponse
+
 from django.shortcuts import render
 
+from blogs.models import Blog, Category
+
 def home(request):
-    return render(request , "index.html")
+    featured_blog = Blog.objects.filter(is_featured=True,status='Published')
+    blogs = Blog.objects.filter(is_featured=False, status='Published')
+
+    context =  {
+        'featured_blog' : featured_blog,
+        'blogs' : blogs,
+    }
+    return render(request , "index.html", context)
