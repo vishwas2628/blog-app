fef76e232c5197c87df86d936a4ea507a68bf3d9
Vishwas Chourasiya <vishwaschourasiya@gmail.com>
2026-02-15 12:01:42 +0530

    further developmwnt in templates " add context_processors for categories add url for categories nav bar

--- blog_main/views.py
+++ blog_main/views.py
@@ -4,12 +4,10 @@ from django.shortcuts import render
 from blogs.models import Blog, Category
 
 def home(request):
-    categories = Category.objects.all()
     featured_blog = Blog.objects.filter(is_featured=True,status='Published')
     blogs = Blog.objects.filter(is_featured=False, status='Published')
 
     context =  {
-        'categories' : categories,
         'featured_blog' : featured_blog,
         'blogs' : blogs,
     }
--- blogs/views.py
+++ blogs/views.py
@@ -1,22 +1,24 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from blogs.models import Category, Blog
 
 
 # Create your views here.
 def blog_by_category(request, category_id):
-    # category = get_object_or_404(Category, pk=category_id)
 
-    try:
-        category = Category.objects.get(pk=category_id)
-    except Category.DoesNotExist:
-        pass
-    #category = Category.objects.get(pk=category_id)
+    # Use try/except when you want to need custom error handling
+    # try:
+    #     category = Category.objects.get(pk=category_id)
+    # except Category.DoesNotExist:
+    #     # redirect to home page if category not exist
+    #     return redirect('home')
 
-
+    # Use get_object_or_404 when you want to show 404 error page if the category not exists
+    category = get_object_or_404(Category, pk=category_id)
 
     context = {
         'posts': Blog.objects.filter(status='Published',category=category_id).order_by('-created_at'),
-        'category': category
+        'category': category,
+        'title': category.category_name,
     }
     return render(request,'blog_by_category.html', context)
