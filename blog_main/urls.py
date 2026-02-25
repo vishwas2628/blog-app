39240616f66e7c7f6318f9d681af7c434e5bdb58
Vishwas Chourasiya <vishwaschourasiya@gmail.com>
2026-02-25 09:05:16 +0530

    commit for random pick just see that commit console work\  - how it looks when commit\  - how we send it

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -32,4 +32,7 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
+
+    # Dashboard urls
+    path('dashboard/', include('dashboard.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@
 import os
 import sys
 
-
+i = 0
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog_main.settings')
