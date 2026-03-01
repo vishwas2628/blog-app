7178d9a785dffefd1496c12a9ed652bc2b4196b1
Vishwas Chourasiya <vishwaschourasiya@gmail.com>
2026-03-01 19:43:58 +0530

    Dashboard mostly completed here

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
+    path('categories/', views.categories, name='categories'),
+    path('categories/add/', views.add_category, name='add_category'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,4 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from blogs.models import Blog,Category
+from django.contrib.auth.decorators import login_required
+from .forms import CategoryForm
 
+@login_required(login_url='login')
 def dashboard(request):
-    return render(request, 'dashboard/dashboard.html')
+    category_count = Category.objects.all().count()
+    blog_count = Blog.objects.all().count()
+
+    context = {
+        'category_count': category_count,
+        'blog_count': blog_count
+    }
+    return render(request, 'dashboard/dashboard.html', context)
+
+def categories(request):
+    return render(request, 'dashboard/categories.html')
+
+def add_category(request):
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('categories')
+    form = CategoryForm()
+
+    context= {
+        'form': form
+    }
+    return render(request, 'dashboard/add_category.html', context)
