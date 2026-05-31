bbc6a0a88981914ae02d0357573d324301a09bec
Vishwas Chourasiya <vishwaschourasiya@gmail.com>
2026-05-31 19:36:09 +0530

    writting commit is sy=uch boaring task

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -11,10 +11,13 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
@@ -31,6 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jet',
     'blogs.apps.BlogsConfig',
     'assignments.apps.AssignmentsConfig',
     'django.contrib.admin',
@@ -131,4 +135,3 @@ MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
-
